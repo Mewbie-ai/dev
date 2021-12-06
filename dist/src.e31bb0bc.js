79df8acf78fe4c80397e828a51db92f1a7fb1bf3
Gabriel Andreas <gabrielandreas96@gmail.com>
2021-12-06 09:38:46 +0700

    update some styles + image sizes

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -8151,7 +8151,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57394" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59001" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
